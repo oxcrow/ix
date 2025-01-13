334cd356e588f96e783e22b9f28a32d7ee7179f4
oxcrow <oxcrow@proton.me>
2025-01-14 04:42:35 +0530

    mod: method to read file
    - improve code

--- ixc/lib/file.ml
+++ ixc/lib/file.ml
@@ -3,6 +3,7 @@ let unit = ()
 (** Read the contents of file into a string. *)
 let read_file_content filename : string =
   let file = open_in_bin filename in
-  let content = really_input_string file (in_channel_length file) in
+  let file_length = in_channel_length file in
+  let content = really_input_string file file_length in
   content
 ;;
