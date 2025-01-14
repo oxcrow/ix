102d49071f4d0ee52560923723660a1b1f33225b
oxcrow <oxcrow@proton.me>
2025-01-15 03:22:43 +0530

    add: silence annoying loc ast node info
    - a "hack" to silence the annoying loc node printouts

--- ixc/lib/ixast.ml
+++ ixc/lib/ixast.ml
@@ -1,3 +1,11 @@
+type loc = Loc of int * int
+
+(* Silence annoying Location output when printing AST. *)
+let show_loc _ = ""
+let pp_loc _ _ = ()
+
+type id = Id of string * loc [@@deriving show { with_path = false }]
+
 type executable = Executable of statements list
 [@@deriving show { with_path = false }]
 
@@ -8,33 +16,24 @@ and statements =
   | StatementEnum
   | StatmentValue of types * id * expressions * loc
   | StatementReturn of expressions
-[@@deriving show { with_path = false }]
 
 and expressions =
   | ExpressionOperation of expressions list * loc
   | ExpressionInvocation of id * arguments list * loc
   | ExpressionTerminal of terminals * loc
-[@@deriving show { with_path = false }]
 
 and blocks = BlockStatement of statements list * loc
-[@@deriving show { with_path = false }]
 
 and arguments =
   | ArgumentDefinition of types * id * loc
   | ArgumentInvocation of expressions list * loc
-[@@deriving show { with_path = false }]
 
 and terminals =
   | IntVal of int
   | FloatVal of float
   | IdVal of id
-[@@deriving show { with_path = false }]
 
 and types =
   | TypeVoid
   | TypeInt
   | TypeFloat
-[@@deriving show { with_path = false }]
-
-and id = Id of string * loc [@@deriving show { with_path = false }]
-and loc = Loc of int * int [@@deriving show { with_path = false }]
