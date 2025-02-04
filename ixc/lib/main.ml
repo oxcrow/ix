de2bc0d9aef5a105ac45602f84e33b8e0dfda129
oxcrow <oxcrow@proton.me>
2025-02-04 16:48:33 +0530

    add: read toml configuration file
    - Use otoml library to read our ix.toml build configuration file
    - For now I was able to extract the name field value
    - In future I was to store our ast within toml file

--- ixc/lib/main.ml
+++ ixc/lib/main.ml
@@ -7,9 +7,19 @@ let print_ast (ast : Ixast.executable) =
   write ("> " ^ Ixast.show_executable ast ^ "\n")
 ;;
 
+let unwrap_error x =
+  match x with
+  | Ok y -> y
+  | Error _ -> failwith "Unwrap failed!"
+;;
+
 (** Driver code *)
 let main =
   header;
+  let config = Otoml.Parser.from_file "ix.toml" in
+  let name =
+    Otoml.find_result config Otoml.get_string [ "src"; "name" ] |> unwrap_error
+  in
   let code = File.read_file_content "main.ix" in
   let ast =
     match Parser.parse code with
@@ -18,7 +28,9 @@ let main =
       x
     | Ok x -> x
   in
-  let _ = print_ast ast in
+  write ("Compiling: " ^ name);
+  print_ast ast;
+  ignore config;
   ignore code;
   ignore ast;
   unit
