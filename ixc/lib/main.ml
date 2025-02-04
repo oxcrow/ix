a3018855f827713dbea5e87b3292a4d896e95640
oxcrow <oxcrow@proton.me>
2025-02-04 16:56:43 +0530

    mod: improve driver code
    - It is annoying to do error handling all the time,
      thus I implemented a method to unwrap results quickly
    - Similarly I implemented methods to extract string data from toml
    - Refactor lil bit

--- ixc/lib/main.ml
+++ ixc/lib/main.ml
@@ -1,34 +1,30 @@
 let unit = ()
 let write line = print_endline line
-let header = write "* ix *\n"
+let header = write " * ix *\n"
 
 (** Print Ix AST for debugging. *)
 let print_ast (ast : Ixast.executable) =
-  write ("> " ^ Ixast.show_executable ast ^ "\n")
+  write (" + " ^ Ixast.show_executable ast ^ "\n")
 ;;
 
-let unwrap_error x =
+(** Unwrap result or crash *)
+let unwrap_result x =
   match x with
   | Ok y -> y
   | Error _ -> failwith "Unwrap failed!"
 ;;
 
+(** Extract string value from toml table *)
+let get_toml_string config x = Otoml.find_result config Otoml.get_string x
+
 (** Driver code *)
 let main =
   header;
   let config = Otoml.Parser.from_file "ix.toml" in
-  let name =
-    Otoml.find_result config Otoml.get_string [ "src"; "name" ] |> unwrap_error
-  in
+  let name = get_toml_string config [ "src"; "name" ] |> unwrap_result in
   let code = File.read_file_content "main.ix" in
-  let ast =
-    match Parser.parse code with
-    | Error x ->
-      let _ = write "Fatal error: Failed to parse code." in
-      x
-    | Ok x -> x
-  in
-  write ("Compiling: " ^ name);
+  let ast = Parser.parse code |> unwrap_result in
+  write (" + Compiling: " ^ name);
   print_ast ast;
   ignore config;
   ignore code;
