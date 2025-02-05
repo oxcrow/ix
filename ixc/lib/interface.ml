ab865b23e25e251d70f551116df085b1804428a3
oxcrow <oxcrow@proton.me>
2025-02-05 22:47:09 +0530

    mod: improve
    - Improve how our code

--- ixc/lib/interface.ml
+++ ixc/lib/interface.ml
@@ -1,10 +1,19 @@
 let unit = ()
 let write line = print_endline line
+let unwrap result = Error.unwrap_result result
+
+(* Pretty printers *)
 let print_string_list = Lexer.print_string_list
-let unwrap_result result = Error.unwrap_result result
 let print_ast_node_list nodes string_of_node = nodes |> List.map string_of_node |> print_string_list
 let print_statement_node_list nodes = print_ast_node_list nodes Ixast.show_statements
 
+(** Data stored within interface of each module or file.
+
+  We want to store this data to ensure that our incremental
+  compilation system can access the data easily and use it to
+  accelerate and parallelise our compilation.
+
+  The idea is to simplify our work later on. *)
 type interfaces =
   | InterfaceModule of
       { uses : interfaces list
@@ -20,30 +29,33 @@ type interfaces =
 let create_statement_use_interface ast =
   let accessor = Walker.get_statement_use in
   let nodes = Walker.walk_ixast_nodes accessor ast in
+
   let extract_id_path node =
     match node with
     | Ixast.Id (path, _) -> path
   in
+
   let extract_use_path node =
     match node with
     | Ixast.StatementUse x ->
       let paths = x.path in
       let paths = paths |> List.map extract_id_path in
       paths
-    | _ -> failwith "Impossible"
+    | _ -> failwith "Failed to extract StatementUse interfaces."
   in
+
   let extract use_nodes =
     let data = use_nodes |> List.map extract_use_path in
     data
   in
+
   let paths = extract nodes in
   let uses = InterfaceUse { path = paths } in
   Ok [ uses ]
 ;;
 
 let create_module_interface ast =
-  let ui = create_statement_use_interface ast |> unwrap_result in
+  let ui = create_statement_use_interface ast |> unwrap in
   let mi = InterfaceModule { uses = ui; functions = []; structs = []; enums = [] } in
-  mi |> show_interfaces |> write;
   Ok mi
 ;;
--- ixc/lib/main.ml
+++ ixc/lib/main.ml
@@ -1,6 +1,6 @@
 let unit = ()
 let write line = print_endline line
-let unwrap_result result = Error.unwrap_result result
+let unwrap result = Error.unwrap_result result
 let header = write " * ix *\n"
 
 (** Print Ix AST for debugging. *)
@@ -13,14 +13,14 @@ let get_toml_string config x = Otoml.find_result config Otoml.get_string x
 let main =
   header;
   let config = Otoml.Parser.from_file "ix.toml" in
-  let name = get_toml_string config [ "src"; "name" ] |> unwrap_result in
-  write (" + Compiling: " ^ name);
+  let name = get_toml_string config [ "src"; "name" ] |> unwrap in
 
   let code = File.read_file_content "main.ix" in
-  let ast = Parser.parse code |> unwrap_result in
-  let mi = Interface.create_module_interface ast |> unwrap_result in
+  let ast = Parser.parse code |> unwrap in
+  let mi = Interface.create_module_interface ast |> unwrap in
 
   ignore config;
+  ignore name;
   ignore code;
   ignore ast;
   ignore mi;
