a7cb722ec906363b681aa2779f8801c2652ca8fb
oxcrow <oxcrow@proton.me>
2025-02-07 15:09:56 +0530

    add: improve result module
    - Add methods to easily check errors

--- ixc/src/result.hh
+++ ixc/src/result.hh
@@ -7,4 +7,16 @@ template<typename T>
 struct Result {
     T data;
     Error error;
+    bool isOk() const;
+    bool isError() const;
 };
+
+template<typename T>
+bool Result<T>::isOk() const {
+    return error.isOk();
+}
+
+template<typename T>
+bool Result<T>::isError() const {
+    return error.isError();
+}
