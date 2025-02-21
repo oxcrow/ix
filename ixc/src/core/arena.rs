51f484a8d84828d76e08feecd44d44d60e66c3c9
oxcrow <oxcrow@proton.me>
2025-02-22 01:30:22 +0530

    x

---
+++ ixc/src/core/arena.rs
@@ -0,0 +1,12 @@
+/// Arena interface reconfiguration
+///
+/// Naming scheme and organization used by bumpallo is sub-optimal.
+/// Thus they have been reconfigured to be easier to understand and use.
+pub mod Arena {
+	pub use bumpalo::Bump as Allocator;
+
+	pub use bumpalo::collections::String;
+	pub use bumpalo::collections::Vec;
+
+	pub use bumpalo::vec;
+}
--- ixc/src/frontend/parser/ast.rs
+++ ixc/src/frontend/parser/ast.rs
@@ -3,20 +3,8 @@ use std::convert::identity;
 use anyhow::ensure;
 use anyhow::Result;
 
+use crate::core::arena::Arena;
 use enum_as_inner::EnumAsInner;
-
-/// Arena interface reconfiguration
-///
-/// Naming scheme and organization used by bumpallo is sub-optimal.
-/// Thus they have been reconfigured to be easier to understand and use.
-pub mod Arena {
-	pub use bumpalo::Bump as Allocator;
-
-	pub use bumpalo::collections::String;
-	pub use bumpalo::collections::Vec;
-
-	pub use bumpalo::vec;
-}
 
 /// Abstract syntax tree
 pub struct Ast<'arena> {
